13c3442e4e06064704bc7125d3bff2fa497ad3c4
Mansi Yadav <139609682+FreeSpirit11@users.noreply.github.com>
2023-08-31 13:41:53 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My website</title>
+
+  <link rel="stylesheet" href="./style.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <div class="poster"> 
+    <img class="motivation-image" src="./assets\images/daenerys.jpeg" alt="daenerys holding an egg"/>
+    <h1>that special moment</h1>
+    <p>When you find the perfect avocado at the supermarket.</p>
+  </div>
+</body>
+</html>
